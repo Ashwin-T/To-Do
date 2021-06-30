b6b9ba89b9157fbba56d47cca41543d3eaeb3af7
Ashwin Talwalkar <atalwalkar719@gmail.com>
2021-06-29 22:27:37 -0700

    Completed and hosted

---
+++ server/backend.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000
+
+
+app.use(express.static(__dirname + '/build'));
+
+app.get('/', (req, res)=> {
+  res.sendFile(__dirname + '/build/index.html');
+})
+
+app.listen(PORT,()=>{console.log("Server starting on " + PORT)});
